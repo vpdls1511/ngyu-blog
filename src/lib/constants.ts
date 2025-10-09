add50d4aad13091a18b8d3cf0cbd27ca61134ae6
nGyu <vpdls1511@gmail.com>
2025-10-10 01:16:57 +0900

    refactor : 이미지 경로 수정 __ 2

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
-export const HOME_OG_IMAGE_URL = "/assets/preview/homepage-og-image.png";
+export const HOME_OG_IMAGE_URL = "https://ngyu.me/assets/preview/homepage-og-image.png";
